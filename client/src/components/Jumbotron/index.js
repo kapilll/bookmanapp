cd5bc96fb1702925a2a343d3bfe4155bc719201b
kapilll <kapilstarksaiyan@gmail.com>
2021-10-31 21:16:20 +0530

    Full commit

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Jumbotron() {
+    return (
+    <div className='jumbotron jumbotron-fluid bg-success text-white text-center'>
+        <span className='display-4 '>
+            BookMan Search
+        </span>
+       
+    </div>
+    )
+}
+
+export default Jumbotron;
